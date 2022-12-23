7ba96c0f51224d31e8a1d061135b1739fa2213bd
bartoszswitalski <bartoszswitalski@icloud.com>
2022-12-23 11:04:36 +0100

    add day 22

--- day 22/solution.py
+++ day 22/solution.py
@@ -1,21 +1,22 @@
+import re
 from enum import Enum
 
 rows = open('input.txt').read().splitlines()
 
 
 class Direction(Enum):
-    LEFT = 'left',
-    RIGHT = 'right',
-    UP = 'up',
-    DOWN = 'down'
+    LEFT = 2
+    RIGHT = 0
+    UP = 3
+    DOWN = 1
 
 
 class Tile:
-    def __init__(self, x, y, isWall=False, neighbors=None):
+    def __init__(self, x, y, isWall=False):
         self.x = x
         self.y = y
         self.isWall = isWall
-        self.neighbors: dict[Direction, Tile] or None = neighbors
+        self.neighbors: dict[Direction, Tile] or None = {}
 
 
 def getNodes() -> dict[(int, int), Tile]:
@@ -34,41 +35,65 @@ def getNodes() -> dict[(int, int), Tile]:
         if (node.x - 1, node.y) in nodes:
             node.neighbors[Direction.LEFT] = (nodes[(node.x - 1, node.y)])
         else:
-            wrappedNeighborCoords = list(filter(lambda c: c[1] == node.y, list(nodes.keys()))).sort(key=lambda c: c[0])[
-                -1]
+            wrappedNeighborCoords = \
+                sorted(list(filter(lambda c: c[1] == node.y, list(nodes.keys()))), key=lambda c: c[0])[
+                    -1]
             node.neighbors[Direction.LEFT] = nodes[wrappedNeighborCoords]
 
         # right
         if (node.x + 1, node.y) in nodes:
             node.neighbors[Direction.RIGHT] = (nodes[(node.x + 1, node.y)])
         else:
-            print(node)
-            wrappedNeighborCoords = list(filter(lambda c: c[1] == node.y, list(nodes.keys()))).sort(key=lambda c: c[0])[
-                0]
+            wrappedNeighborCoords = \
+                sorted(list(filter(lambda c: c[1] == node.y, list(nodes.keys()))), key=lambda c: c[0])[
+                    0]
             node.neighbors[Direction.RIGHT] = nodes[wrappedNeighborCoords]
 
         # up
         if (node.x, node.y - 1) in nodes:
             node.neighbors[Direction.UP] = (nodes[(node.x, node.y - 1)])
         else:
-            wrappedNeighborCoords = list(filter(lambda c: c[0] == node.x, list(nodes.keys()))).sort(key=lambda c: c[1])[
-                -1]
+            wrappedNeighborCoords = \
+                sorted(list(filter(lambda c: c[0] == node.x, list(nodes.keys()))), key=lambda c: c[1])[
+                    -1]
             node.neighbors[Direction.UP] = nodes[wrappedNeighborCoords]
 
         # down
         if (node.x, node.y + 1) in nodes:
             node.neighbors[Direction.DOWN] = (nodes[(node.x, node.y + 1)])
         else:
-            wrappedNeighborCoords = list(filter(lambda c: c[0] == node.y, list(nodes.keys()))).sort(key=lambda c: c[1])[
-                0]
+            wrappedNeighborCoords = \
+                sorted(list(filter(lambda c: c[0] == node.x, list(nodes.keys()))), key=lambda c: c[1])[
+                    0]
             node.neighbors[Direction.DOWN] = nodes[wrappedNeighborCoords]
 
     return nodes
 
 
 def part1():
-    nodes = getNodes()
-    return nodes
+    nodes: dict[(int, int), Tile] = getNodes()
+    steps = re.split('(\d+)', rows[-1])[1:-1]
+
+    currentNode: (int, int) = sorted(list(filter(lambda c: c[1] == 0, nodes.keys())), key=lambda c: c[0])[0]
+    direction = Direction.RIGHT
+    for step in steps:
+        # print('before step', currentNode, step)
+        if not step.isnumeric():
+            direction = Direction((direction.value + 1) % 4) if step == 'R' else Direction((direction.value - 1) % 4)
+            # print('new direction', direction)
+            continue
+        for _ in range(int(step)):
+            if nodes[currentNode].neighbors[direction].isWall:
+                # print('hit wall')
+                break
+            nextNode = nodes[currentNode].neighbors[direction]
+            # print('moving', direction, nextNode.x, nextNode.y)
+            currentNode = (nextNode.x, nextNode.y)
+        # print('after step', currentNode)
+
+    colIndex, rowIndex = currentNode
+    print(currentNode)
+    return 1000 * (rowIndex + 1) + 4 * (colIndex + 1) + direction.value
 
 
 def part2():
